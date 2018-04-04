10b7a769443c5138b259f4be26ff8743fa00ab7c
unfaiyted <unfaiyted@gmail.com>
2018-04-04 15:12:15 -0500

    panel design with css backend

--- js/main.js
+++ js/main.js
@@ -1,31 +1,58 @@
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function () {
+        $('<img />').attr('src',this).appendTo('body').css('display','none');
+    });
+}
+
+// Usage:
+
+preload([
+    'images/time.jpg',
+    'images/24hours.jpg',
+    'images/hipster.jpg',
+    'images/large-pg-img.jpg'
+]);
 
 
-let  i = 0;
-setInterval(function() {
 
-    let images= ['time.jpg','24hours.jpg','hipster.jpg','large-pg-img.jpg'];
-    (i === images.length-1) ? i = 0 : i++;
+$(window).on('load', function() { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(600).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(600).css({'overflow':'visible'});
 
-    console.log(i);
-    var image = $('#image-holder');
-    image.fadeOut(2000, function () {
-        image.css("background", "url('images/" + images[i] + "')");
-        image.css("background-size", "cover");
-        image.fadeIn(2000);
+
+
+    let  i = 0;
+    setInterval(function() {
+
+        let images= ['time.jpg','24hours.jpg','hipster.jpg','large-pg-img.jpg'];
+        (i === images.length-1) ? i = 0 : i++;
+
+        console.log(i);
+        var image = $('#image-holder');
+        image.fadeOut(2000, function () {
+            image.css("background", "url('images/" + images[i] + "')");
+            image.css("background-size", "cover");
+            image.fadeIn(2000);
+
+        });
+
+    }, 6000);
+
+
+    $(document).scroll(function() {
+
+        if($(this).scrollTop() >= 70) {
+            $('.bg-custom-dark').css('background-color','#1C1C1D');
+        } else {
+            $('.bg-custom-dark').css('background-color','transparent');
+
+        }
 
     });
 
-}, 6000);
 
 
-$(document).scroll(function() {
 
-    if($(this).scrollTop() >= 70) {
-        $('.bg-custom-dark').css('background-color','#1C1C1D');
-    } else {
-        $('.bg-custom-dark').css('background-color','transparent');
-
-    }
-
-})
+});
 
